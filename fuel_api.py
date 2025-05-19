a1ce0b6ce00ff05a8a063256b2db3840d4230db8
ily-akmal <akmalifthikar@gmail.com>
2025-05-19 22:11:09 +0530

    Minor

--- fuel_api.py
+++ fuel_api.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify
+import os
+
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
+    app.run(host="0.0.0.0", port=port)
 
 @app.route("/fuel", methods=["GET"])
 def get_fuel_prices():
